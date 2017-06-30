d728fe33c49af62f184f4c7f91f4e6203d0e31f9
Jared Hanson <hansonj@mit.edu>
2017-06-29 20:27:36 -0700

    Added initial navbar fixed-top functionality

--- website/js/index.js
+++ website/js/index.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+  // Adding event listeners
+  var navBar = $('.nav-bar').first();
+  $(window).scroll(function() {
+    const windowHeight = $(window).height();
+    const currentScroll = $(window).scrollTop();
+    var fixedTop = navBar.hasClass('fixed-top');
+    if ((currentScroll > windowHeight) && (!fixedTop)) {
+        navBar.addClass('fixed-top');
+        $('body').addClass('navbar-offset');
+    } else if ((currentScroll < windowHeight) && (fixedTop)) {
+        navBar.removeClass('fixed-top');
+        $('body').removeClass('navbar-offset');
+    }
+  })
+})
 function storyShortener (story) {
     var storyLength = story.length;
     if (storyLength > 200) {
