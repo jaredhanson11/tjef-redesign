f7d6326aac9532ec5df739f37412b9d2e9ce5984
Jared Hanson <jred0011@gmail.com>
2020-04-22 14:18:31 -0700

    Rearranged so TJEF Meals is first

--- website/js/spotlight_data.js
+++ website/js/spotlight_data.js
@@ -13,23 +13,6 @@ const portrait = "portrait"
 var slideShow = [
     {
         "imgs": [
-            new StoryMedia("./img/spotlight.png","Eric Sodomka (center right) and Okke Schrijvers (center left)", landscape)
-        ],
-        "desc":
-        `
-Eric Sodomka and Okke Schrijvers are senior scientists at Facebook headquarters in Menlo Park. Eric spent countless hours to get four
-young Nigerian young academics visas, conference fees, and flights to
-come to Oxford to attend the 14th conference on web and internet
-economics, a conference on mechanism design. Okke (long standing
-John Ernest Fellow and member of TJEF’s College of Mentors) connected
-them to The John Ernest Foundation, which provided their conference fees
-to help them reach their goals. This project was conceived 1 month ago.
-Talk about adding value in a timely manner!  Conference was held at Oxford
-University in mid-December, 2018.
-        `
-    },
-    {
-        "imgs": [
             new StoryMedia("./img/meals/meals-1.jpg", "", portrait),
             new StoryMedia("./img/meals/meals-2.png", "", portrait)
         ],
@@ -47,6 +30,23 @@ Should you wish to help, please notate MEALS on your donation.
 <br>
 <br>
 <a href="https://espostosmeals.com" target="_blank">espostosmeals.com</a>
+        `
+    },
+    {
+        "imgs": [
+            new StoryMedia("./img/spotlight.png","Eric Sodomka (center right) and Okke Schrijvers (center left)", landscape)
+        ],
+        "desc":
+        `
+Eric Sodomka and Okke Schrijvers are senior scientists at Facebook headquarters in Menlo Park. Eric spent countless hours to get four
+young Nigerian young academics visas, conference fees, and flights to
+come to Oxford to attend the 14th conference on web and internet
+economics, a conference on mechanism design. Okke (long standing
+John Ernest Fellow and member of TJEF’s College of Mentors) connected
+them to The John Ernest Foundation, which provided their conference fees
+to help them reach their goals. This project was conceived 1 month ago.
+Talk about adding value in a timely manner!  Conference was held at Oxford
+University in mid-December, 2018.
         `
     },
     {
