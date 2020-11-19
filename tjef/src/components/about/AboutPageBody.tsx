8281a69028de0e0be16d51d1bae7a4e1d1a799c5
Jared Hanson <jred0011@gmail.com>
2020-11-18 19:24:54 -0800

    Added about us page icons

--- tjef/src/components/about/AboutPageBody.tsx
+++ tjef/src/components/about/AboutPageBody.tsx
@@ -1,5 +1,8 @@
 /** @jsx jsx */
 import {css, jsx} from "@emotion/core"
+import different from "../../imgs/different_icon.png"
+import meaning from "../../imgs/meaning_icon.png"
+import mentoring from "../../imgs/mentoring_icon.png"
 import defaultStyles from "../../styles"
 import AboutSection from "./AboutSection"
 
@@ -16,16 +19,25 @@ export default function AboutPageBody(props: AboutPageBodyProps) {
   return (
     <div css={containerCss}>
       <AboutSection
-        title={"The Value of Mentoring"}
+        title={"Value of Mentoring"}
         body={
-          "For TJEF, our real wealth is our College of Mentors, a group of individuals who have the care and enthusiasmt to pass on their knowledge and expertise to John Ernest Fellows who have similiar interests or aspirations. The may be college counselors for a lost student. They may have found success in a carreer field and can sahre their path with another generation. They may produce an internship for a Fellow. The College of Mentors is the most important step in making dreams into realities."
+          "For TJEF, our real wealth is our College of Mentors, a group of individuals whop have the care and enthusiasm to pass on their knowledge and expertise to John Ernest Fellows who have similar interests or aspirations.  They may be college counselors for a lost student.  They may have found success in a career field and can share their path with another generation.  They may produce an internship for a Fellow.  The College of Mentors is the most important step in making dreams into realities."
         }
+        icon={mentoring}
       />
       <AboutSection
-        title={"The Value of Mentoring"}
+        title={"How is TJEF Different?"}
         body={
-          "For TJEF, our real wealth is our College of Mentors, a group of individuals who have the care and enthusiasmt to pass on their knowledge and expertise to John Ernest Fellows who have similiar interests or aspirations. The may be college counselors for a lost student. They may have found success in a carreer field and can sahre their path with another generation. They may produce an internship for a Fellow. The College of Mentors is the most important step in making dreams into realities."
+          "We believe organizing ourselves under a specific dedication is restrictive to whose lives we are able to add value to.  To only support Fellows with a very particular interest would be exclusionary to others that we may be capable of supporting with our College of Mentors and additional resources.  Pairing the value of mentoring from this College of Mentors with grants from our John Ernest Fellowship Fund amplify the value added to give TJEF the ability to do quite a bit with the resources of a small organization."
         }
+        icon={different}
+      />
+      <AboutSection
+        title={"Meaning of a John Ernest Fellow"}
+        body={
+          "What does it mean to become a John Ernest Fellow? It might seem like an easy question, but I am sure that every Fellow would give you a different response. When I look at the definition of mentoring, which, according to Wikipedia, is “a relationship in which a more experienced or more knowledgeable person helps to guide a less experienced or less knowledgeable person,” I find it to be incomplete. It is a relevant description, but it doesn’t acknowledge the fact that both the mentor and the less knowledgeable person learn through this process."
+        }
+        icon={meaning}
       />
     </div>
   )
--- tjef/src/components/about/AboutSection.tsx
+++ tjef/src/components/about/AboutSection.tsx
@@ -5,9 +5,11 @@ import defaultStyles from "../../styles"
 type AboutSectionProps = {
   title: string
   body: string
+  icon: string
 }
 
 export default function AboutSection(props: AboutSectionProps) {
+  let iconHeight = 100
   let containerCss = css(defaultStyles.roundedCorners, {
     width: "95%",
     maxWidth: 1000,
@@ -16,14 +18,21 @@ export default function AboutSection(props: AboutSectionProps) {
     },
     marginRight: "auto",
     marginLeft: "auto",
+    marginTop: iconHeight / 2,
+    backgroundColor: defaultStyles.colors.lightGreen,
     paddingLeft: 20,
     paddingRight: 20,
-    paddingTop: 40,
     paddingBottom: 40,
-    backgroundColor: defaultStyles.colors.lightGreen,
   })
+  let iconContainerCss = css({
+    position: "relative",
+    top: -iconHeight / 2,
+    height: iconHeight / 2,
+  })
+  let imgCss = css({height: "200%"})
   let titleCss = css(defaultStyles.header2, {
     display: "inline-block",
+    paddingTop: 20,
     paddingLeft: 10,
     paddingRight: 10,
   })
@@ -31,6 +40,9 @@ export default function AboutSection(props: AboutSectionProps) {
   let bodyCss = css(defaultStyles.body, {})
   return (
     <div css={containerCss}>
+      <div css={iconContainerCss}>
+        <img src={props.icon} alt={`${props.title} img`} css={imgCss} />
+      </div>
       <div css={titleCss}>{props.title}</div>
       <div css={bodyCss}>
         <hr css={lineBreakCss} />
