4baf937cdade205c6677dafad4b4b218763768b6
Jared Hanson <jred0011@gmail.com>
2020-11-25 02:15:49 -0800

    Fix nasty safari bug

--- tjef/src/components/common/PageBody.tsx
+++ tjef/src/components/common/PageBody.tsx
@@ -8,7 +8,8 @@ type PageBodyProps = PropsWithChildren<{background?: "offwhite" | "none"}>
 export default function PageBody(props: PageBodyProps) {
   let bodyCss = css({
     width: "100vw",
-    height: "100vh",
+    height: "100%",
+    minHeight: "100vh",
     overflow: "scroll",
     display: "flex",
     flexDirection: "column",
